634ac09addb55f135952cbd593a58d4a69518471
Alexia A-J <alexia_allard-jacquin@hotmail.fr>
2024-05-22 11:59:27 +0200

    Add data to DOM

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,10 +7,23 @@ function photographerTemplate(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", name);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const location = document.createElement('p');
+        location.textContent = city + ", " + country;
+        location.classList.add('location');
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+        tag.classList.add('tagline');
+        const rate = document.createElement('p');
+        rate.textContent = price + "€/jour";
+        rate.classList.add('rate');
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tag);
+        article.appendChild(rate);
         return (article);
     }
     return { name, picture, getUserCardDOM }
